e901c66877fd95f4e92cfd00f4def10ac2fe6a24
Jomily <jomilyanv@gmail.com>
2022-12-08 00:47:35 +0200

    Dtos
    add History requests:
    - `CreateHistoryRequest`
    - `GetHistoryRequests`

---
+++ DoorsLock/DoorsApi/Dtos/History/Requests/CreateHistoryRequest.cs
@@ -0,0 +1,20 @@
+﻿using DoorsApi.Models;
+
+namespace DoorsApi.Dtos.History
+{
+    public record CreateHistoryRequest
+    {
+        public Guid BuildingId { get; set; }
+        public Guid DoorId { get; set; }
+        public Guid UserId { get; set; }
+        public Status Status { get; set; }
+
+        public CreateHistoryRequest(Guid buildingId, Guid doorId, Guid userId, Status status)
+        {
+            BuildingId = buildingId;
+            DoorId = doorId;
+            UserId = userId;
+            Status = status;
+        }
+    }
+}
---
+++ DoorsLock/DoorsApi/Dtos/History/Requests/GetHistoryRequest.cs
@@ -0,0 +1,18 @@
+﻿using DoorsApi.Models;
+
+namespace DoorsApi.Dtos.History
+{
+    public class GetHistoryRequest
+    {
+        public Guid BuildingId { get; set; }
+        public Guid? DoorId { get; set; }
+        public Guid? UserId { get; set; }
+        public DateTime? DateTime { get; set; }
+        public Status? Status { get; set; }
+
+        public GetHistoryRequest(Guid buildingId)
+        {
+            BuildingId = buildingId;
+        }
+    }
+}
